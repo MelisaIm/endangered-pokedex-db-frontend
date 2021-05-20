acc2a6b08aec02b848b23cacdeb4ebeead79e34c
Melisa Im <melisaevelynim@gmail.com>
2021-05-19 21:25:14 -0700

    Fix captivity places table

--- src/pages/CaptivityPlaces.js
+++ src/pages/CaptivityPlaces.js
@@ -4,11 +4,13 @@ import Table from '../TableBase';
 export default class CaptivityPlaces extends Table {
 
     renderTable() {
+        console.log(this.props.data);
         return this.props.data.map(place => 
             // data row
             <tr key={place.zooId}>
                 <td>{place.zooId}</td>
                 <td>{place.zooName}</td>
+                <td>{place.zooCity}</td>
                 <td>{place.zooState}</td>
                 <td>{place.zooCountry}</td>
                 <td>{place.zooCoordinates}</td>
