9c1c05df81e62b93e36c1dc3752a85e163bd4c70
Melisa Im <mim@adobe.com>
2021-05-27 09:04:28 -0700

    Add post nonprofits

--- src/pages/Nonprofits.js
+++ src/pages/Nonprofits.js
@@ -3,6 +3,28 @@ import Table from '../TableBase';
 
 export default class Nonprofits extends Table {
 
+    onClickAdd(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        var object = {};
+        formData.forEach((value, key) => object[key] = value);
+
+        axios.post(`${endpoint}insert/nonprofits`, object).then(((res) => window.location.reload())).catch(() => {
+            alert('Insert to endangered species table failed');
+        });
+    }
+
+    renderForm() {
+        const inputs = ['nonprofitName', 'nonprofitWebsite'];
+        return (<form onSubmit={(e) => this.onClickAdd(e)} className="createForm">
+                    {inputs.map((key, index) => <div key={`div-${index}-${key}`} className="formItem">
+                            <label key={`label-${index}-${key}`}>{key}</label><br/>
+                            <input required key={`input-${index}-${key}`} type="text" id={key} name={key}/><br/><br/>
+                    </div>)}
+            <input type="submit" value="Add Row"/>
+        </form>) 
+    }
+
     renderTable() {
         return this.props.data.map(nonprofit => 
             // data row
