f1f0876f31b70a0d21d5cccbe885374432ff9995
Melisa Im <mim@adobe.com>
2021-05-27 09:28:55 -0700

    add endangered nonprofits

--- src/pages/EndangeredNonprofits.js
+++ src/pages/EndangeredNonprofits.js
@@ -10,23 +10,34 @@ export default class EndangeredNonprofits extends Table {
         const formData = new FormData(e.target);
         var object = {};
         formData.forEach((value, key) => object[key] = value);
-
-        axios.post(`${endpoint}insert/nonprofits`, object).then(((res) => window.location.reload())).catch(() => {
+        axios.post(`${endpoint}insert/endangered-nonprofits`, object).then(((res) => window.location.reload())).catch(() => {
             alert('Insert to endangered species table failed');
         });
     }
 
     renderForm() {
-        const inputs = ['nonprofitName', 'nonprofitWebsite'];
+        // const inputs = ['endangeredSpecies', 'nonprofits'];
         return (<form onSubmit={(e) => this.onClickAdd(e)} className="createForm">
-                    {inputs.map((key, index) => <div key={`div-${index}-${key}`} className="formItem">
-                            <label key={`label-${index}-${key}`}>{key}</label><br/>
-                            <input required key={`input-${index}-${key}`} type="text" id={key} name={key}/><br/><br/>
-                    </div>)}
+                    <div className="formItem">
+                    <label for="endangeredSpecies">Choose an endangered species:</label>
+                        <select name="animalId" id="animals">
+                        {this.props.endangeredSpecies.map((value, index) => {
+                            return <option value={value.animalId}>{`${value.commonName} (${value.scientificName})`}</option>
+                        })}
+                        </select> 
+                    </div>
+                    <div className="formItem">
+                    <label for="nonprofits">Choose a nonprofit:</label>
+                        <select name="nonprofitId" id="nonprofit">
+                        {this.props.nonprofits.map((value, index) => {
+                            return <option value={value.nonprofitId}>{`${value.nonprofitName}`}</option>
+                        })}
+                        </select> 
+                    </div>
             <input type="submit" value="Add Row"/>
         </form>) 
     }
-    
+
     renderTable() {
         return this.props.data.map((dataPoint, index) => 
             // data row
