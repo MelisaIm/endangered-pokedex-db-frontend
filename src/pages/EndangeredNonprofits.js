7340d70f314d499083910547379fe4f27c6e55fd
Melisa Im <mim@adobe.com>
2021-05-27 09:13:16 -0700

    Pass secondary props for select values

--- src/pages/EndangeredNonprofits.js
+++ src/pages/EndangeredNonprofits.js
@@ -5,6 +5,28 @@ import axios from 'axios';
 
 export default class EndangeredNonprofits extends Table {
 
+    onClickAdd(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        var object = {};
+        formData.forEach((value, key) => object[key] = value);
+
+        axios.post(`${endpoint}insert/nonprofits`, object).then(((res) => window.location.reload())).catch(() => {
+            alert('Insert to endangered species table failed');
+        });
+    }
+
+    renderForm() {
+        const inputs = ['nonprofitName', 'nonprofitWebsite'];
+        return (<form onSubmit={(e) => this.onClickAdd(e)} className="createForm">
+                    {inputs.map((key, index) => <div key={`div-${index}-${key}`} className="formItem">
+                            <label key={`label-${index}-${key}`}>{key}</label><br/>
+                            <input required key={`input-${index}-${key}`} type="text" id={key} name={key}/><br/><br/>
+                    </div>)}
+            <input type="submit" value="Add Row"/>
+        </form>) 
+    }
+    
     renderTable() {
         return this.props.data.map((dataPoint, index) => 
             // data row
