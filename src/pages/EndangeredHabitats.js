75535c25a18f98116fa8231d971ea4605d4320f7
Melisa Im <melisaevelynim@gmail.com>
2021-05-27 20:59:37 -0700

    fix endangered habitat insert

--- src/pages/EndangeredHabitats.js
+++ src/pages/EndangeredHabitats.js
@@ -9,7 +9,7 @@ export default class EndangeredHabitats extends Table {
         e.preventDefault();
         const formData = new FormData(e.target);
         var object = {};
-        formData.forEach((value, key) => object[key] = value);
+        formData.forEach((value, key) => object[key] = parseInt(value));
         axios.post(`${endpoint}insert/endangeredHabitats`, object).then(((res) => window.location.reload())).catch(() => {
             alert('Insert to endangeredHabitats table failed');
         });
@@ -27,7 +27,7 @@ export default class EndangeredHabitats extends Table {
                     </div>
                     <div className="formItem">
                     <label htmlFor="nativeHabitats">Choose a native habitat:</label>
-                        <select name="nativeHabitatId" id="nativeHabitat">
+                        <select name="habitatId" id="nativeHabitat">
                         {this.props.nativeHabitats.map((value, index) => {
                             return <option value={value.habitatId} key={index}>{`${value.country} | ${value.biome}`}</option>
                         })}
@@ -44,7 +44,8 @@ export default class EndangeredHabitats extends Table {
                 <td>{dataPoint.animalId}</td>
                 <td>{dataPoint.habitatId}</td>
                 <td>{dataPoint.location}</td>
-                <td><button onClick={this.onClickUpdate}>Save Changes</button><button onClick={this.onClickDelete}>Delete</button></td>
+                <td><button onClick={this.onClickUpdate}>Save Changes</button>
+                <button onClick={() => this.onClickDelete('endangeredHabitats', {data: {animalId: dataPoint.animalId, habitatId: dataPoint.habitatId}})}>Delete</button></td>
             </tr>
         );
     }
